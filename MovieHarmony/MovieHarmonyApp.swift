b254f846c3398068f7da98f239488303b8444872
m.francuzov <maxfrancuzov@gmail.com>
2024-02-05 10:55:49 +0300

    Initial Commit

---
+++ MovieHarmony/MovieHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHarmonyApp.swift
+//  MovieHarmony
+//
+//  Created by Максим Французов on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
